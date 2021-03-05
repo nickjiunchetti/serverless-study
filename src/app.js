3f72e1f73b54d4f698c7c47e99ad65076c2d27d0
nickj <nickjiunchetti@gmail.com>
2021-03-05 13:37:51 -0300

    added http status to response

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.get('/', (req, res) => {
-	res.send('Get Sucessfull')
+	res.status(200).send('Get Sucessfull')
 })
 
 module.exports = app
